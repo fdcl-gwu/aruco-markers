b81c199db4c74f0c1c1dea124f9f0745599a05ad
Kani-FDCL <kanishkegb@gwu.edu>
2019-04-23 18:00:35 -0400

    move camera to see front, top, and left faces

--- opengl_cube/src/main.cpp
+++ opengl_cube/src/main.cpp
@@ -201,7 +201,7 @@ int main(void)
     glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
     // Camera matrix
     glm::mat4 View = glm::lookAt(
-        glm::vec3(4, 3, -3), // Camera is at (4,3,-3), in World Space
+        glm::vec3(3, 3, 3), // Camera is at (4,3,-3), in World Space
         glm::vec3(0, 0, 0),  // and looks at the origin
         glm::vec3(0, 1, 0)   // Head is up (set to 0,-1,0 to look upside-down)
     );
