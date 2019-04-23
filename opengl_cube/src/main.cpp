4d0ee288c128bb994955b52cec545cf9a769f1bb
Kani-FDCL <kanishkegb@gwu.edu>
2019-04-23 16:53:51 -0400

    shows empty screen

--- opengl_cube/src/main.cpp
+++ opengl_cube/src/main.cpp
@@ -37,4 +37,22 @@ int main()
         fprintf(stderr, "Failed to initialize GLEW\n");
         return -1;
     }
+
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    do
+    {
+        // Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Draw nothing, see you in tutorial 2 !
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    } // Check if the ESC key was pressed or the window was closed
+    while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+           glfwWindowShouldClose(window) == 0);
 }
